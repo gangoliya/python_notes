695bb8fe6ed20a1e6a07e271d6ecff3fa8690409
gangoliya <gangoliyaprashu@gmail.com>
2025-02-16 11:01:22 +0530

    Added while loop assignment files

---
+++ python_teaching/assignments/loop_7.py
@@ -0,0 +1,8 @@
+# Modify the following code to stop when  x becomes 3 using a  break statement
+x = 10
+
+while x>0:
+    print(x)
+    if x == 3:
+        break
+    x -= 1
---
+++ python_teaching/assignments/loop_8.py
@@ -0,0 +1,6 @@
+# Write a program that keeps asking the user to enter a number until they enter a negative number.
+
+x = 0
+
+while x >= 0:
+    x = int(input("Enter negative integer: "))
