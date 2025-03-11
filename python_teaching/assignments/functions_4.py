d6106e76589a40e693d02d5ef24f9483ac482050
gangoliya <gangoliyaprashu@gmail.com>
2025-03-11 08:15:28 +0530

    Updated day 14 files

--- python_teaching/assignments/functions_4.py
+++ python_teaching/assignments/functions_4.py
@@ -1,9 +1,9 @@
 # Factorial of a number
 
 def fact(num):
-    sum = 0
+    fct = 0
     for i in range(1, num+1):
-        sum += i
+        fct *= i
     return sum
 
 
---
+++ python_teaching/functions/python_functions_1.py
@@ -0,0 +1,54 @@
+# Method Overloading
+
+# def my_func(num):
+#     pass
+
+# def my_func(num, num1 = 1):
+#     pass
+
+
+# Positional-Only Arguments
+
+# def my_func(x, y, /):
+#     return x
+# print(my_func(4)) -- This will work
+# print(my_func(x = 4)) -- This will give an error
+
+
+
+# Keyword-Only Arguments
+
+# def my_func(*, x):
+#     return x
+
+# print(my_func(x = 4)) # This will work
+# print(my_func(4)) # This will give an error
+
+# Combine Positional-Only and Keyword-Only Arguments
+# def my_func(a, b, /, *, c, d):
+#     return a, b, c, d
+
+# print(my_func(3, 4, c = 5, d = 6))
+
+# Recursion
+
+def tri_recursion(k):
+    if(k>0):
+        result = k + tri_recursion(k-1) 
+        # 6 + tri_recursion(5) 
+        # 5 + tri_recusrion(4) 
+        # 4 + tri_recusrion(3)
+        # 3 + tri_recusrion(2)
+        # 2 + tri_recusrion(1) # 2 + 1 = 3
+        # 1 + tri_recusrion(0) # 1 + 0 = 1
+        print(result)
+    else:
+        result = 0
+
+    return result
+
+print(tri_recursion(6))
+
+
+# factorial using normal loop
+# factorial using recursion
