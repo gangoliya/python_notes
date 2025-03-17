ceeba209767e1ba2c15e7c328a4f2e8b8beabac2
gangoliya <gangoliyaprashu@gmail.com>
2025-03-17 08:09:34 +0530

    Updated few files

--- python_teaching/assignments/functions_4.py
+++ python_teaching/assignments/functions_4.py
@@ -1,10 +1,10 @@
 # 7. Factorial of a number
 
-def fact(num):
-    fct = 0
-    for i in range(1, num+1):
-        fct *= i
-    return sum
+def fact(num): # 5
+    result = 1
+    for i in range(1, num+1): # 1, 6 (1 - 5)
+        result *= i # fct = fct * i
+    return result
 
 
 number = int(input("Enter number for factorial: "))
